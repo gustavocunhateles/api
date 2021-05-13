f29ce80eb394ed0c170a0dd8f5bcd31bbb6b2fcf
Gustavo Cunha Teles <42899628+gustavocunhateles@users.noreply.github.com>
2021-05-13 11:27:58 -0300

    Add files via upload

---
+++ build_20210318/precache-manifest.ef74f2dbd60e80faa4cabefae3bdc1ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fbf377e5f52752bef9a",
+    "url": "/static/css/main.ba6f107f.chunk.css"
+  },
+  {
+    "revision": "5fbf377e5f52752bef9a",
+    "url": "/static/js/main.d6d3c858.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14c2751a5aa30dfdf120",
+    "url": "/static/js/2.336c0a7a.chunk.js"
+  },
+  {
+    "revision": "04e5ec22082c767c5164723489458e36",
+    "url": "/index.html"
+  }
+];
